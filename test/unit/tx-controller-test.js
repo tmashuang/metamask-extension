1d261ab7f86904e0e685eab4d8a7d869766e2bd3
Thomas Huang <thomas.b.huang@gmail.com>
2017-06-19 09:25:55 -0700

    Testing async/await with sign transaction

--- test/unit/tx-controller-test.js
+++ test/unit/tx-controller-test.js
@@ -22,7 +22,7 @@ describe('Transaction Controller', function () {
       provider: { _blockTracker: new EventEmitter()},
       blockTracker: new EventEmitter(),
       ethQuery: new EthQuery(new EventEmitter()),
-      signTransaction: (ethTx) => new Promise((resolve) => {
+      signTransaction: async (ethTx) => await new Promise((resolve) => {
         ethTx.sign(privKey)
         resolve()
       }),
@@ -164,6 +164,7 @@ describe('Transaction Controller', function () {
       }
       txController.on('1:rejected', noop)
       txController.setTxStatusRejected(1)
+      done()
     })
   })
 
