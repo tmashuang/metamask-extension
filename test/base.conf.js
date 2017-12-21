37ca3c64417209d76b34c467b795e565b1334e9e
Dan <danjm.com@gmail.com>
2017-12-21 13:06:26 -0330

    Karma nocache?

--- test/base.conf.js
+++ test/base.conf.js
@@ -54,6 +54,8 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    nocache: true,
   }
 }
