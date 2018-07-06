9b9904da3bc0d09e2f6eeb17dd5f61696ee29255
Thomas <thomas.b.huang@gmail.com>
2018-07-06 00:14:31 -0700

    Test i18n-helper with fetch-mock

---
+++ test/unit/ui/i18-helper.spec.js
@@ -0,0 +1,40 @@
+const assert = require('assert')
+const fetchMock = require('fetch-mock')
+const { getMessage, fetchLocale } = require('../../../ui/i18n-helper')
+const enLocale = require('../../../app/_locales/en/messages.json')
+
+
+describe('i18n', () => {
+
+  before(() => {
+    fetchMock.get('*', enLocale)
+  })
+
+  after(() => {
+    fetchMock.restore()
+  })
+
+  it('#fetchLocale', async () => {
+    const response = await fetchLocale('en')
+    assert.equal(typeof response, 'object')
+    assert.equal(response.builtInCalifornia.message, 'MetaMask is designed and built in California.')
+  })
+
+  describe('#getMessage', () => {
+
+    // This is a representation of  state.localMessages
+    const localeMessages = {
+      current: enLocale,
+    }
+
+    it('#getMessage', () => {
+      const i18nGetMessage = getMessage(localeMessages, 'builtInCalifornia')
+      assert.equal(i18nGetMessage, 'MetaMask is designed and built in California.')
+    })
+
+    it('#getMessage returns empty string with empty args', () => {
+      assert.equal(getMessage(), '')
+    })
+
+  })
+})
