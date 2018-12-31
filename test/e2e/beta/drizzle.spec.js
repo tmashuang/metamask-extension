62d5ccb49f0e0d862ca54edba2817971a56f52e6
Thomas <thomas.b.huang@gmail.com>
2018-12-30 22:08:18 -0800

    see if notification is up

--- test/e2e/beta/drizzle.spec.js
+++ test/e2e/beta/drizzle.spec.js
@@ -269,17 +269,18 @@ describe('MetaMask', function () {
 
       await delay(regularDelayMs)
       const approveButton = await findElement(driver, By.xpath(`//button[contains(text(), 'Connect')]`))
-      await approveButton.click()
+      assert.fail()
+      // await approveButton.click()
     })
 
-    it('should be able to detect our eth address', async () => {
-      // Check if address exposed
-      await driver.switchTo().window(dapp)
-      await delay(regularDelayMs)
+    // it('should be able to detect our eth address', async () => {
+    //   // Check if address exposed
+    //   await driver.switchTo().window(dapp)
+    //   await delay(regularDelayMs)
 
-      const addressElement = await findElement(driver, By.css(`.pure-u-1-1 h4`))
-      const addressText = await addressElement.getText()
-      assert(addressText.match(/^0x[a-fA-F0-9]{40}$/))
-    })
+    //   const addressElement = await findElement(driver, By.css(`.pure-u-1-1 h4`))
+    //   const addressText = await addressElement.getText()
+    //   assert(addressText.match(/^0x[a-fA-F0-9]{40}$/))
+    // })
   })
 })
