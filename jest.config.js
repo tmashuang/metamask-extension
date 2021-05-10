a71a0bc676231b8310147f2cf63ebb38612ef11a
Thomas <thomas.b.huang@gmail.com>
2021-05-10 08:19:05 -0700

    Add exempted files that need to be rewritten from ground up from mocha to jest

--- jest.config.js
+++ jest.config.js
@@ -13,8 +13,18 @@ module.exports = {
   },
   setupFiles: ['./test/setup.js', './test/env.js'],
   setupFilesAfterEnv: ['./test/jest/setup.js'],
+<<<<<<< HEAD
   testMatch: [
     '<rootDir>/ui/**/?(*.)+(test).js',
     '<rootDir>/shared/**/?(*.)+(test).js',
+=======
+  testMatch: ['<rootDir>/**/*.test.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/app/scripts/metamask-controller.test.js',
+    '<rootDir>/app/scripts/controllers/transactions/index.test.js',
+    '<rootDir>/app/scripts/controllers/network/pending-middleware.test.js',
+    '<rootDir>/app/scripts/controllers/permissions/permissions-middleware.test.js',
+    '<rootDir>/test/unit-global/',
+>>>>>>> b93452754 (Add exempted files that need to be rewritten from ground up from mocha to jest)
   ],
 };
